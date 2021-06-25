b2fce36fd6fac3ed6518f4280ee349586f219d12
mlopez <lopezmatthew87@gmail.com>
2021-06-24 22:51:18 -0400

    Engineer test passed

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -2,9 +2,20 @@
 const Employee = require('./Employee');
 
 class Engineer extends Employee {
-    constructor() {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this.github = github;
+
 
     };
+
+    getRole() {
+        return 'Engineer';
+    }
+
+    getGithub() {
+        return this.github;
+    }
 };
 
 module.exports = Engineer;
